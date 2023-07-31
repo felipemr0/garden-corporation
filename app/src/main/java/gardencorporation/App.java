ebad652b4fc1c6a62eca80493a8d59f407abf3d9
Felipe Rêgo <feliperego@Felipes-Mini.lan>
2023-07-31 20:05:11 +0100

    gradle setup

---
+++ app/src/main/java/gardencorporation/App.java
@@ -0,0 +1,11 @@
+package gardencorporation;
+
+import GUI.MainPage;
+
+public class App {
+    
+    public static void main(String[] args) {
+        MainPage mainPage = new MainPage();
+        mainPage.setVisible(true);
+    }
+}
